7fd280a1e960709361750d3eff315b8b924491f1
Artur <sanakoev.2004@mail.ru>
2024-02-26 10:34:10 +0300

    Добавьте файлы проекта.

---
+++ HillCode/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace HillCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV
+            // 
+            this.DGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.ColumnHeadersVisible = false;
+            this.DGV.Location = new System.Drawing.Point(12, 12);
+            this.DGV.Name = "DGV";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGV.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DGV.RowHeadersVisible = false;
+            this.DGV.RowTemplate.Height = 120;
+            this.DGV.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.DGV.Size = new System.Drawing.Size(360, 360);
+            this.DGV.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(416, 42);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(301, 107);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // textBoxEncrypt
+            // 
+            this.textBoxEncrypt.Location = new System.Drawing.Point(416, 184);
+            this.textBoxEncrypt.Multiline = true;
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(301, 107);
+            this.textBoxEncrypt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Вводимый текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Зашифрованый текст";
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(528, 346);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 5;
+            this.buttonEncrypt.Text = "button1";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.DGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonEncrypt;
+    }
+}
+
---
+++ HillCode/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HillCode
+{
+    public partial class Form1 : Form
+    {
+        private int n;
+        private char[] alphabetDecrype;
+        private Dictionary<char, int> alphabetEncrype = new Dictionary<char, int>();
+        private string inputText;
+        private int[,] matrixA;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DGV.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            DGV.Height = 260;
+            n = 3;
+
+            for (int i = 0; i < n; i++)
+            {
+                DGV.Columns.Add("Column" + i.ToString(), "");
+                DGV.Rows.Add();
+                DGV.Rows[i].Height = DGV.Height / n - 1;
+            }
+
+            alphabetDecrype = "ZABCDEFGHIJKLMNOPQRSTUVWXY".ToCharArray();
+
+            for (int i = 0; i < alphabetDecrype.Length; i++)
+            {
+                alphabetEncrype.Add(alphabetDecrype[i], i);
+            }
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            GetMatrixA();
+            inputText = textBoxInput.Text.ToUpper();
+
+            while (inputText.Length % n != 0)
+                inputText += "Z";
+
+            HillEncrype(inputText);
+        }
+
+        private void HillEncrype(string s)
+        {
+            char[] input = s.ToCharArray();
+            int t = s.Length / n;
+            string res = "";
+
+            for (int i = 0; i < t; i++)
+            {
+                var vector = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    vector[j] = alphabetEncrype[input[i * n + j]];
+                }
+
+                var codeVector = new int[n];
+                codeVector = MatrixMultiplication(matrixA, vector);
+
+                for (int j = 0; j < codeVector.Length; j++)
+                {
+                    res += alphabetDecrype[codeVector[j]];
+                }
+            }
+
+            textBoxEncrypt.Text = res;
+        }
+
+        private int[] MatrixMultiplication(int[,] a, int[] b)
+        {
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += (a[i, j] * b[j]) % alphabetDecrype.Length;
+                }
+                res[i] = sum % alphabetDecrype.Length;
+            }
+            return res;
+        }
+
+        public void GetMatrixA()
+        {
+            matrixA = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrixA[i, j] = int.Parse(DGV.Rows[i].Cells[j].Value.ToString());
+        }
+    }
+}
